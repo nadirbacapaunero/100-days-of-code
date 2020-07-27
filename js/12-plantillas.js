81799bfa7c9c5720cfbae2701cd8f5470b6f08fb
nadirbacapaunero <nbacapaunero@gmail.com>
2020-07-27 17:50:13 -0300

    plantillas en javascript

---
+++ js/12-plantillas.js
@@ -0,0 +1,11 @@
+var nombre = prompt("Coloca tu nombre");
+var apellido= prompt("Coloca tu apellido");
+var texto= `
+
+    <h1>Hola que tal</h1>
+    <h3>Mi nombre es: ${nombre}</h3>
+    <h3>Mi apellido es: ${apellido}</h3>
+    `;
+
+    document.write(texto);
+    
