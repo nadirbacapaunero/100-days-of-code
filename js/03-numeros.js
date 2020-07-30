9db5397ee100dd5dca2871d12cc1b68746602967
nadirbacapaunero <nbacapaunero@gmail.com>
2020-07-29 23:59:11 -0300

    numeros en js dia 3

---
+++ js/03-numeros.js
@@ -0,0 +1,14 @@
+/* numeros decimales */
+
+var ourDecimal = 5.7;
+var myDecimal= 8.2;
+
+// multiplicaciones
+
+var product = 2.0 * 2.5;
+
+//division
+var quotient = 4.4 / 2.0;
+
+//El operador restante% da el resto de la división de dos números
+var remainder= 11 % 3;
