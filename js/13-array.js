3dfb40fae77990f6cc5a3343abde59d94806ad68
nadirbacapaunero <nbacapaunero@gmail.com>
2020-07-30 21:01:54 -0300

    arrays dia4

---
+++ js/13-array.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//Arrays, Arreglos, Matrices una coleccion de datos que puede tener una variable
+
+var nombre = 'Nadir'
+var nombres= ["Lucas","Matias", "Mario", 67, true, false]
+//index         0      1          2      3      4    5
+
+var lenguajes = new Array ("PHP", "JS", "Go", "JAVA");
+console.log(nombres);
+console.log(lenguajes);
+
+console.log(nombres[1]);
+// sacar la cantidad de elementos
+console.log(nombres.length)
+
+// como pedir elemento en un alert
+
+var elemento= parseInt(prompt("Que elemento del array quieres?", 0));
+//alert(nombres[elemento]);
+
+if(elemento >=nombres.length){
+    alert("Introduce un numero correcto menor que " + nombres.length);
+} else {
+    alert("El usuario seleccionado es: " +nombres[elemento]);
+}
+
+lenguajes.forEach((elemento)=>{
+    document.write("<li>" +elemento+"</li>")
+});
